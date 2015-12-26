ceb8acf2912cdbfa37747a6d746d7907edc53790
Chiedo <chiedo@chie.do>
2015-12-26 15:02:54 -0500

    Code cleanup

--- src/doc-doc-goose.js
+++ src/doc-doc-goose.js
@@ -1,14 +1,14 @@
 'use strict';
 require('babel-register');
-let fs = require('fs')
-
-let data = require('../examples/advanced.js');
-
+let fs         = require('fs');
 let handlebars = require('handlebars');
+let data       = require('../examples/advanced.js');
 
-let x = fs.readFileSync(`${__dirname}/templates/default.handlebars`).toString();
-let template = handlebars.compile(x);
-let html = template(data);
 
+// parse the HTML using the data
+let templateFile = fs.readFileSync(`${__dirname}/templates/default.handlebars`).toString();
+let template     = handlebars.compile(templateFile);
+let html         = template(data);
+
+//save the file
 console.log(html);
-
