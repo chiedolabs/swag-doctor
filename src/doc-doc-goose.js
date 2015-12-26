ffb2dfbd2fdb821e1aee4e6d8a2233f7c33e407c
Chiedo <chiedo@chie.do>
2015-12-26 16:03:13 -0500

    Basic inputs and outputs working

--- src/doc-doc-goose.js
+++ src/doc-doc-goose.js
@@ -1,14 +1,20 @@
 'use strict';
 require('babel-register');
-let fs         = require('fs');
-let handlebars = require('handlebars');
-let data       = require('../examples/advanced.js');
+let argv = require('yargs').argv;
+let path= require('path');
 
+let cwd = path.resolve('.');
 
-// parse the HTML using the data
-let templateFile = fs.readFileSync(`${__dirname}/templates/default.handlebars`).toString();
-let template     = handlebars.compile(templateFile);
-let html         = template(data);
+if(argv.i && argv.o) {
+  let fs         = require('fs');
+  let handlebars = require('handlebars');
+  let data       = require(`${cwd}/${argv.i}`);
 
-//save the file
-fs.writeFile('docs/index.html', html, 'utf8');
+  // parse the HTML using the data
+  let templateFile = fs.readFileSync(`${__dirname}/templates/default.handlebars`).toString();
+  let template     = handlebars.compile(templateFile);
+  let html         = template(data);
+
+  //save the file
+  fs.writeFile(`${cwd}/${argv.o}/index.html`, html, 'utf8');
+}
