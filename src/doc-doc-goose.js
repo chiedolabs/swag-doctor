c560fad1d1f69e1b8410956b737563f997fcb3db
Chiedo <chiedo@chie.do>
2015-12-26 15:00:55 -0500

    More improved proof of concep

--- src/doc-doc-goose.js
+++ src/doc-doc-goose.js
@@ -1,15 +1,14 @@
 'use strict';
 require('babel-register');
+let fs = require('fs')
 
 let data = require('../examples/advanced.js');
 
 let handlebars = require('handlebars');
 
-let template = handlebars.compile(`
-{{#each paths}}
-    Key: {{@key}} Value = {{this}}
-{{/each}}
-`);
+let x = fs.readFileSync(`${__dirname}/templates/default.handlebars`).toString();
+let template = handlebars.compile(x);
 let html = template(data);
 
 console.log(html);
+
