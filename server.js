5ef07d03bb4f866775dee60409ef1e2bc6138c6c
Chiedo <chiedo@chie.do>
2015-12-28 11:44:27 -0500

    Simplified further

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 let env  = process.env.NODE_ENV || 'development';
 let fs   = require('fs');
-let path = require('path');
 let express = require('express');
 
 fs.stat('.env', (err, stat) => {
@@ -28,15 +27,7 @@ if(env === 'development') {
 }
 
 // Allows for the use of other static resources
-app.use('/static', express.static('dist/static'));
-
-app.set('view engine', 'ejs');
-
-app.get('*', (req, res) => {
-  res.render(path.join(__dirname, 'index.ejs'), {
-    API_URL: process.env.API_URL || 'http://localhost:8000',
-  });
-});
+app.use('/', express.static('dist'));
 
 app.listen(PORT, (err) => {
   if (err) {
