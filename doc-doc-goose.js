a7b028f775f8969334590f33344983167e2304dd
Chiedo <chiedo@chie.do>
2015-12-28 11:51:52 -0500

    Successful outputting of data

--- doc-doc-goose.js
+++ doc-doc-goose.js
@@ -1,9 +1,11 @@
 'use strict';
 require('babel-register');
-let argv = require('yargs').argv;
-let path = require('path');
-let _    = require('lodash');
-let fs   = require('fs');
+let argv   = require('yargs').argv;
+let path   = require('path');
+let _      = require('lodash');
+let fs     = require('fs');
+let ncp    = require('ncp').ncp;
+let appDir = path.dirname(require.main.filename);
 
 if(argv.i && argv.o) {
   // Get the execution directory
@@ -23,6 +25,12 @@ if(argv.i && argv.o) {
     }
   }
 
-  // Save the docs
-  fs.writeFile(`${cwd}/${outputDir}/data.json`, JSON.stringify(data) , 'utf8');
+  // Copy the react app from this package to the user's output dir
+  ncp(`${appDir}/dist/`, `${cwd}/${outputDir}/`, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    // Save the doc data file
+    fs.writeFile(`${cwd}/${outputDir}/data.json`, JSON.stringify(data) , 'utf8');
+  });
 }
