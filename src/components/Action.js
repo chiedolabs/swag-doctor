c0ebaa188fc5cd5ef3769b79ad25e07d12148f8a
Chiedo <chiedo@chie.do>
2015-12-28 16:48:26 -0500

    More progress

--- src/components/Action.js
+++ src/components/Action.js
@@ -11,18 +11,47 @@ class Action extends Component{
   render(){
     const { action } = this.props;
 
+    let bodyParams;
+    let urlParams;
+    let bodyParamsOutput;
+    let urlParamsOutput;
+
+    if(action.params){
+      if(action.params.body) {
+        bodyParams = action.params.body;
+      }
+
+      if(action.params.url) {
+        bodyParams = action.params.url;
+      }
+    }
+
+    if(urlParams){
+      urlParamsOutput = (
+        <div>
+          <h4>URL Parameters</h4>
+          <div>{JSON.stringify(urlParams)}</div>;
+        </div>
+      );
+    }
+
+    if(bodyParams){
+      bodyParamsOutput = (
+        <div>
+          <h4>Parameters</h4>
+          <div>{JSON.stringify(bodyParams)}</div>
+        </div>
+      );
+    }
+
     return (
       <div>
         <Button bsStyle="success" className="pull-left">{action.method}</Button>
         <h3 id={`${generateID(action.name)}`}>
           &nbsp;{action.name}
         </h3>
-        <div>
-          <br/>
-          <h4>URL Parameters</h4>
-          <h4>Parameters</h4>
-          {JSON.stringify(action.params)}
-        </div>
+        {urlParamsOutput}
+        {bodyParamsOutput}
       </div>
     );
   }
